df1d0aac6d4e358f39a27252fc1343913ca23a8f
yundanangel <yundanangel@outlook.com>
2016-06-08 20:33:47 -0400

    new

---
+++ Noogie.java
@@ -0,0 +1,19 @@
+
+
+public class Noogie {
+		private int _meow = 0;
+		
+		public Noogie(int meow) {
+			_meow = meow;
+		}
+		
+		public int coogie() {
+			return 2 * _meow;
+		}
+
+		public int schmoogie() {
+			return _meow / 2;
+		}
+		
+}
+
